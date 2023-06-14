805fa57ee9cef54fc0bc046b903259099acc6869
oliver-gomes <ogomesphoto@gmail.com>
2023-06-13 20:59:32 -0400

    rendering example

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+"user client"; // for client components
+// by default it will be server component
+
+import React from "react";
+
+const AboutPage = () => {
+  console.log("server component");
+  return <div>AboutPage</div>;
+};
+
+export default AboutPage;
